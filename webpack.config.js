ab79284af66b243f87d533c1f0ff2167a87cb79d
Luke Westby <hello@lukewestby.com>
2017-05-04 11:32:10 -0500

    remove laravel and get things running on webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const debug = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'build/js'),
+    filename: 'index.js'
+  },
+  plugins: debug ?
+    [
+      new HtmlPlugin({
+        filename: 'index.html',
+        template: './index.html',
+        inject: true
+      })
+    ] :
+    [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ],
+};
