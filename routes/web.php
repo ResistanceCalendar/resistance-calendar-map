d4ae07fd6f53cf069bbbdaf1cb37287028d8498a
aaghevli <arash.aghevli@gmail.com>
2018-03-10 20:40:23 -0800

    Update the routes to call RC-API vs use a database

--- routes/web.php
+++ routes/web.php
@@ -20,32 +20,39 @@ Route::post('import/OSDI', 'ImportController@importOSDI');
 Route::post('import/events-etl', 'ImportController@importEventsEtl');
 
 Route::get('events', function() {
+  Cache::flush();
 	return response()->json(Cache::remember('events', 60, function () {
-	    $events = [];
-	    foreach(App\CalendarEvent::get() as $event) {
-	    	if(!isset($event['event']['formatType'])) {
-	    		if(isset($event['event']['location']['location']['coordinates'])) {
-	    			array_push($events, [
-	    				'start_datetime' => $event['event']['start_date'],
-	    				'url'            => 'https://facebook.com/events/' . substr($event['event']['identifiers'][0], 9),
-	    				'venue'          => '',
-	    				'group'          => null,
-	    				'title'          => $event['event']['title'],
-	    				'lat'            => isset($event['event']['location']['location']['coordinates'][1]) ? $event['event']['location']['location']['coordinates'][1] : '',
-	    				'lng'            => isset($event['event']['location']['location']['coordinates'][0]) ? $event['event']['location']['location']['coordinates'][0] : '',
-	    				'supergroup'     => 'Indivisible',
-	    				'event_type'     => 'Event',
-	    				'attending'      => isset($event['event']['total_accepted']) ? $event['event']['total_accepted'] : null,
-	    			]);
-	    		}
-	    	}
-	    	else if($event['event']['formatType'] == 'events-etl') {
-	    		array_push($events,
-	    			(object) array_merge((array) $event['event'], (array) ['attending' => 0])
-	    		);
-	    	}
+    $events = [];
+    $now = urlencode(date('\'Y-m-d\''));
+    error_log($now);
+    $query = "\$filter=start_date%20gt%20$now";
+    $url = "https://resistance-calendar.herokuapp.com/v1/events?page=0&per_page=3000&$query";
+    error_log($url);
+    $response = file_get_contents($url);
+    if ($response !== false) {
+      error_log("response okay");
+      $responseJson = json_decode($response, true);
+      error_log("response decoded");
+	    foreach($responseJson['_embedded']['osdi:events'] as $event) {
+    		if(isset($event['location']['location'])) {
+          $eventUrl = 'https://www.resistancecalendar.org/event/' . $event['_id'];
+    			array_push($events, [
+    				'start_datetime' => $event['start_date'],
+    				'url'            => $eventUrl,
+    				'venue'          => '',
+    				'group'          => null,
+    				'title'          => $event['title'],
+    				'lat'            => isset($event['location']['location']['latitude']) ? $event['location']['location']['latitude'] : '',
+    				'lng'            => isset($event['location']['location']['longitude']) ? $event['location']['location']['longitude'] : '',
+    				'supergroup'     => 'Indivisible',
+    				'event_type'     => 'Event',
+    				'attending'      => isset($event['total_accepted']) ? $event['total_accepted'] : null,
+    			]);
+    		}
 	    }
-	    return $events;
+    }
+    error_log($url);
+    return $events;
 	}));
 });
 
